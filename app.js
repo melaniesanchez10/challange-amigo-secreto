46ead4742defcdfcf474c77d8ece7bf38f084ceb
melaniesanchez10 <scmelanie10@gmail.com>
2025-02-05 00:48:05 -0600

    Cambio en la funcionalidad de agregarAmigo

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-amigos.push();
-console.log(amigos);
 function agregarAmigo() {
-    let nombreAmigo =document.getElementById('amigo');
-    console.log(nombreAmigo);
+    let amigoNombre = document.getElementById('amigo').value;
+    amigos.push(amigoNombre);
+    console.log(amigos);
+    document.getElementById('amigo').value = ''; 
 }
 function sortearAmigo() {
 
