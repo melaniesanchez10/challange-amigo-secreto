5045241cf9b7681eca7c208758f314bfe20aebc8
melaniesanchez10 <scmelanie10@gmail.com>
2025-02-05 00:34:14 -0600

    Creación de array amigos

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = ["Juan"];
+amigos.push("Hola");
+console.log(amigos);
+function agregarAmigo() {
+    
+}
+function sortearAmigo() {
+
+}
