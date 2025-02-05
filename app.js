ad7b37247631dfd9bc05693c17a11e564319b8bc
melaniesanchez10 <scmelanie10@gmail.com>
2025-02-05 00:40:21 -0600

    Primeras pruebas de funcionabilidad de la función agregarAmigo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = ["Juan"];
-amigos.push("Hola");
+let amigos = [];
+amigos.push();
 console.log(amigos);
 function agregarAmigo() {
-    
+    let nombreAmigo =document.getElementById('amigo');
+    console.log(nombreAmigo);
 }
 function sortearAmigo() {
 
